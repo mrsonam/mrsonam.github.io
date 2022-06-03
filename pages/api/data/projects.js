3c3655b9e1d05ff41c5d1e0867eeff1a1ef60d16
Sonam Wangdi Sherpa <sonamsrp8@gmail.com>
2022-06-03 17:30:30 +0545

    add portfolio

---
+++ pages/api/data/projects.js
@@ -0,0 +1,10 @@
+import boafresh from '../../../public/images/boafresh.png';
+const projects  = [
+  {
+    id: 1,
+    name: 'BoaFresh: Restaurant E-Commerce',
+    image: boafresh
+  }
+]
+
+module.exports = projects;
